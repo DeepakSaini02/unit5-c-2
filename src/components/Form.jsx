478726044892e8724e9b495bba83efed2fcbf7f6
Deepak Saini <91534605+DeepakSaini02@users.noreply.github.com>
2022-01-03 15:16:54 +0530

    recipe form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import './Form.css';
 export const Form=()=>{
 const [form,setForm]=useState(null)
 
@@ -11,11 +11,28 @@ const handleChange=e=>{
     })
 }
 
-    return <form  action="">
+const handleSubmit=(e)=>{
+    e.preventDefault()
+    let res=fetch('http://localhost:3001/recipe',{
+        method:"POST",
+        body:JSON.stringify(form),
+        headers:{
+            'Content-Type':'application/json'
+        }
+    }).then((res)=> res.json()).then((res)=>console.log(res))
+   
+
+}
+    return <div id="recipeDiv">
+ <form onSubmit={handleSubmit} id="recipeForm" action="">
         <input onChange={handleChange} name="title" type="text" placeholder="title" />
         <input onChange={handleChange} name="ingredients" type="text" placeholder="ingredients" />
         <input onChange={handleChange} name="time" type="number" placeholder="time to cook in min"/>
         <input onChange={handleChange} name="image" type="text" placeholder="image url"/>
+        <textarea onChange={handleChange} name="ins" cols="30"  rows="10" placeholder="recipe instruction"></textarea>
         <input type="submit" value="submit" />
     </form>
+    </div>
+    
+   
 }
